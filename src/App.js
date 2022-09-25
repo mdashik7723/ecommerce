df2ee30542cf4265d3e2944b01a89161d0c2cb54
mdashik7723 <mdashik7723@gmail.com>
2022-09-26 01:17:19 +0600

    --

--- src/App.js
+++ src/App.js
@@ -8,10 +8,36 @@ import SingleProduct from "./SingleProduct";
 import Cart from "./Cart";
 import ErrorPage from "./ErrorPage";
 import {GlobalStyle} from "./GlobalStyle";
+import {ThemeProvider} from "styled-components";
 
 const App = () => {
+
+    const theme = {
+        colors:{
+            text: "rgba(29 , 29 , 29 , .8)",
+            white: "#fff",
+            black: "#212529",
+            helper: "#8490ff",
+
+            bg:"#F1F1F1",
+            footer_bg: "#0a1435",
+            btn: "rgba(98, 84, 243)",
+            border: "rgba(98, 84, 243, 0.5)",
+            hr: "#ffffff",
+            gradient:
+            "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%",
+            shadow:
+            "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,rgba(27, 31, 35, 0.15) 0px 0px 0px 1px",
+            shadowSupport: "rgba(0, 0, 0, 0.16) 0px 0px 0px 1px",
+        },
+        media:{
+            mobile:"786",
+            tab: "998px",
+        },
+    };
     return (
         <>
+            <ThemeProvider theme={theme}>
             <Router>
                 <GlobalStyle/>
                 <Routes>
@@ -24,6 +50,7 @@ const App = () => {
                     <Route path="*" element= { <ErrorPage/> } />
                 </Routes>
             </Router>
+            </ThemeProvider>
         </>
     );
 };
--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,36 +1,29 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: "Work Sans", sans-serif;
 }
-
-
 html {
   font-size: 62.5%;
   /* scroll-behavior: smooth; */
   /* 1rem = 10px */
   overflow-x: hidden;
 }
-
 body {
   overflow-x: hidden;
    scrollbar-color: rgb(98 84 243);
     scrollbar-width: thin;
 }
-
 body::-webkit-scrollbar {
   width: 1.5rem;
 }
-
 body::-webkit-scrollbar-track {
    background-color: rgb(24 24 29);
 }
-
 body::-webkit-scrollbar-thumb {
  
   background: #fff;
@@ -38,21 +31,17 @@ body::-webkit-scrollbar-thumb {
     border-radius: 9px;
     background-clip: content-box;
 }
-
 h1,
 h2,
 h3,
 h4 {
    font-family: "Work Sans", sans-serif;
-
 }
-
 h1 {
   color: ${({ theme }) => theme.colors.heading};
   font-size: 6rem;
   font-weight: 900;
 }
-
  h2 {
    color: ${({ theme }) => theme.colors.heading};
    font-size: 4.4rem;
@@ -60,70 +49,54 @@ h1 {
    white-space: normal;
   
   }
-
 h3 {
   font-size: 1.8rem;
   font-weight: 400;
 }
-
 p, button {
   color: ${({ theme }) => theme.colors.text};
   font-size: 1.65rem;
   line-height: 1.5;
   font-weight:400;
 }
-
 a {
   text-decoration: none;
 }
-
 li {
   list-style: none;
 }
-
-
 ${"" /* resuable code section  */}
-
 .container {
   max-width: 120rem;
   margin: 0 auto;
 }
-
 .grid {
   display: grid;
   gap: 9rem;
 }
-
 .grid-two-column {
   grid-template-columns: repeat(2, 1fr);
-
 }
-
 .grid-three-column {
   grid-template-columns: repeat(3, 1fr);
 }
-
 .grid-four-column{
    grid-template-columns: 1fr 1.2fr .5fr .8fr ;
 }
-
 .grid-five-column{
   grid-template-columns: repeat(5, 1fr);
 }
-
   .common-heading {
       font-size: 3.8rem;
       font-weight: 600;
       margin-bottom: 6rem;
       text-transform: capitalize;
     }
-
      .intro-data {
       margin-bottom: 0;
       text-transform: uppercase;
       color: #5138ee;
     }
-
    .caption {
       position: absolute;
       top: 15%;
@@ -135,7 +108,6 @@ ${"" /* resuable code section  */}
       font-size: 1.2rem;
       border-radius: 2rem;
     }
-
 input, textarea{
     max-width: 50rem;
     color: ${({ theme }) => theme.colors.black};
@@ -156,19 +128,16 @@ input, textarea{
     font-size: 1.8rem;
     cursor: pointer;
     }
-
 @media (max-width: ${({ theme }) => theme.media.tab}) {
     .container {
     max-width: 130rem;
     padding: 0 3.2rem;
   }
   }
-
    @media (max-width: ${({ theme }) => theme.media.mobile}) {
        html {
       font-size: 50%;
     }
-
 .grid{
   gap: 3.2rem;
 }
@@ -176,5 +145,4 @@ input, textarea{
           grid-template-columns: 1fr;
         }
     }
-
 `;
