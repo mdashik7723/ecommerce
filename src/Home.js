5e864029497949bfdb820cfb2d3993eb0982d97a
mdashik7723 <mdashik7723@gmail.com>
2022-09-30 01:10:33 +0600

    --

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
 import HeroSection from "./component/HeroSection";
+import Services from "./component/Services";
+import Trusted from "./component/Trusted";
 
 const Home = () => {
     const data = {
         name: "Ashik store",
     };
 
-    return <HeroSection myData={data}/>;
+    return(
+        <>
+            <HeroSection myData={data}/>
+            <Services/>
+            <Trusted/>
+        </>
+    );
+
 };
 
 export default Home;
