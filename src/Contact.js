e5f15cd088afb07cd12b6a8d5148d7c818621bd1
mdashik7723 <mdashik7723@gmail.com>
2022-10-02 02:08:13 +0600

    --

--- src/Contact.js
+++ src/Contact.js
@@ -3,40 +3,44 @@ import styled from "styled-components";
 
 const Contact = () => {
     const Wrapper = styled.section`
-    padding: 9rem 0 5rem 0;
-    text-align: center;
-    .container {
-      margin-top: 6rem;
-      .contact-form {
-        max-width: 50rem;
-        margin: auto;
-        .contact-inputs {
-          display: flex;
-          flex-direction: column;
-          gap: 3rem;
-          input[type="submit"] {
-            cursor: pointer;
-            transition: all 0.2s;
-            &:hover {
-              background-color: ${({ theme }) => theme.colors.white};
-              border: 1px solid ${({ theme }) => theme.colors.btn};
-              color: ${({ theme }) => theme.colors.btn};
-              transform: scale(0.9);
+      padding: 9rem 0 5rem 0;
+      text-align: center;
+
+      .container {
+        margin-top: 6rem;
+
+        .contact-form {
+          max-width: 50rem;
+          margin: auto;
+
+          .contact-inputs {
+            display: flex;
+            flex-direction: column;
+            gap: 3rem;
+
+            input[type="submit"] {
+              cursor: pointer;
+              transition: all 0.2s;
+
+              &:hover {
+                background-color: ${({theme}) => theme.colors.white};
+                border: 1px solid ${({theme}) => theme.colors.btn};
+                color: ${({theme}) => theme.colors.btn};
+                transform: scale(0.9);
+              }
             }
           }
         }
       }
-    }
-  `;
+    `;
 
-    return (
-        <Wrapper>
+    return (<Wrapper>
             <h2 className="common-heading">Contact page</h2>
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d454.7111224652572!2d89.92434321606144!3d24.25331920016589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6f622e2a6414ef3f!2sAshik&#39;s%20Home!5e0!3m2!1sen!2sbd!4v1664652097058!5m2!1sen!2sbd"
                 width="100%"
                 height="400"
-                style={{ border: 0 }}
+                style={{border: 0}}
                 allowFullScreen=""
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"></iframe>
@@ -71,12 +75,11 @@ const Contact = () => {
                             autoComplete="off"
                             placeholder="Enter you message"></textarea>
 
-                        <input type="submit" value="send" />
+                        <input type="submit" value="send"/>
                     </form>
                 </div>
             </div>
-        </Wrapper>
-    );
+        </Wrapper>);
 };
 
 export default Contact;
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
-import { Button } from "../styles/Button";
-import { FaDiscord, FaInstagram, FaYoutube } from "react-icons/fa";
+import {NavLink} from "react-router-dom";
+import {Button} from "../styles/Button";
+import {FaDiscord, FaInstagram, FaYoutube} from "react-icons/fa";
 
 const Footer = () => {
-    return (
-        <>
+    return (<>
             <Wrapper>
                 <section className="contact-short">
                     <div className="grid grid-two-column">
@@ -33,25 +32,24 @@ const Footer = () => {
                         <div className="footer-subscribe">
                             <h3>Subscribe to get important updates</h3>
                             <form action="#">
-                                <input type="email" name="email" placeholder="YOUR E-MAIL" />
-
-                                <input type="submit" value="subscribe" />
+                                <input type="email" name="email" placeholder="YOUR E-MAIL"/>
+                                <input type="submit" value="subscribe"/>
                             </form>
                         </div>
                         <div className="footer-social">
                             <h3>Follow Us</h3>
                             <div className="footer-social--icons">
                                 <div>
-                                    <FaDiscord className="icons" />
+                                    <FaDiscord className="icons"/>
                                 </div>
                                 <div>
-                                    <FaInstagram className="icons" />
+                                    <FaInstagram className="icons"/>
                                 </div>
                                 <div>
                                     <a
                                         href="https://www.youtube.com/channel/UCkJDZQtqRQC-ee9nEhvaWbg"
                                         target="_blank">
-                                        <FaYoutube className="icons" />
+                                        <FaYoutube className="icons"/>
                                     </a>
                                 </div>
                             </div>
@@ -63,7 +61,7 @@ const Footer = () => {
                     </div>
 
                     <div className="footer-bottom--section">
-                        <hr />
+                        <hr/>
                         <div className="container grid grid-two-column ">
                             <p>
                                 @{new Date().getFullYear()} Ashik.All Rights Reserved
@@ -76,46 +74,53 @@ const Footer = () => {
                     </div>
                 </footer>
             </Wrapper>
-        </>
-    );
+        </>);
 };
 
 const Wrapper = styled.section`
   .iSIFGq {
     margin: 0;
   }
+
   .contact-short {
     max-width: 60vw;
     margin: auto;
     padding: 5rem 10rem;
-    background-color: ${({ theme }) => theme.colors.bg};
+    background-color: ${({theme}) => theme.colors.bg};
     border-radius: 1rem;
-    box-shadow: ${({ theme }) => theme.colors.shadowSupport};
+    box-shadow: ${({theme}) => theme.colors.shadowSupport};
     transform: translateY(50%);
+
     .grid div:last-child {
       justify-self: end;
       align-self: center;
     }
   }
+
   footer {
     padding: 14rem 0 9rem 0;
-    background-color: ${({ theme }) => theme.colors.footer_bg};
+    background-color: ${({theme}) => theme.colors.footer_bg};
+
     h3 {
-      color: ${({ theme }) => theme.colors.hr};
+      color: ${({theme}) => theme.colors.hr};
       margin-bottom: 2.4rem;
     }
+
     p {
-      color: ${({ theme }) => theme.colors.white};
+      color: ${({theme}) => theme.colors.white};
     }
+
     .footer-social--icons {
       display: flex;
       gap: 2rem;
+
       div {
         padding: 1rem;
         border-radius: 50%;
-        border: 2px solid ${({ theme }) => theme.colors.white};
+        border: 2px solid ${({theme}) => theme.colors.white};
+
         .icons {
-          color: ${({ theme }) => theme.colors.white};
+          color: ${({theme}) => theme.colors.white};
           font-size: 2.4rem;
           position: relative;
           cursor: pointer;
@@ -123,27 +128,33 @@ const Wrapper = styled.section`
       }
     }
   }
+
   .footer-bottom--section {
     padding-top: 9rem;
+
     hr {
       margin-bottom: 2rem;
-      color: ${({ theme }) => theme.colors.hr};
+      color: ${({theme}) => theme.colors.hr};
       height: 0.1px;
     }
   }
-  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+
+  @media (max-width: ${({theme}) => theme.media.mobile}) {
     .contact-short {
       max-width: 80vw;
       margin: 4.8rem auto;
       transform: translateY(0%);
       text-align: center;
+
       .grid div:last-child {
         justify-self: center;
       }
     }
+
     footer {
       padding: 9rem 0 9rem 0;
     }
+
     .footer-bottom--section {
       padding-top: 4.8rem;
     }
