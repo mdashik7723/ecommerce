0eef85945554721f6db6e773f065a124126a839a
mdashik7723 <mdashik7723@gmail.com>
2022-10-06 02:17:25 +0600

    --

--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -1,9 +1,22 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import {createContext} from "react";
+import axios from "axios";
 
 const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products"
+
 const AppProvider = ({children}) => {
+
+    const getProducts = async (url) => {
+        const res = await axios.get(url);
+        const products = await res.data;
+    }
+
+    useEffect(() => {
+        getProducts(API);
+    },[]);
+
     return(
         <>
         <AppContext.Provider value={{myName:"As Hi K"}}>
