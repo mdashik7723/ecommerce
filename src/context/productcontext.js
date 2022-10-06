9e1271aa42ebec990d340d4b338eecfc12f8c99d
mdashik7723 <mdashik7723@gmail.com>
2022-10-07 01:03:05 +0600

    --

--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -1,25 +1,40 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useReducer} from "react";
 import {createContext} from "react";
 import axios from "axios";
+import reducer from "../reducer/productReducer";
+import {type} from "@testing-library/user-event/dist/type";
 
 const AppContext = createContext();
 
 const API = "https://api.pujakaitem.com/api/products"
 
+const initialState = {
+    isLoading: false,
+    isError:false,
+    products:[],
+    featureProducts: [],
+};
+
 const AppProvider = ({children}) => {
 
+    const [state, dispatch ] = useReducer(reducer, initialState);
     const getProducts = async (url) => {
-        const res = await axios.get(url);
-        const products = await res.data;
-    }
-
+        dispatch({type: "SET_LOADING"});
+        try {
+            const res = await axios.get(url);
+            const products = await res.data;
+            dispatch({ type: "SET_API_DATA", payload: products });
+        } catch (error) {
+            dispatch({ type: "API_ERROR" });
+        }
+    };
     useEffect(() => {
         getProducts(API);
     },[]);
 
     return(
         <>
-        <AppContext.Provider value={{myName:"As Hi K"}}>
+        <AppContext.Provider value={{...state}}>
             {children}
         </AppContext.Provider>;
         </>
--- src/reducer/productReducer.js
+++ src/reducer/productReducer.js
@@ -1,4 +1,3 @@
-import React from "react";
 const ProductReducer = (state, action) => {
     // if (action.type === "SET_LOADING") {
     //   return {
