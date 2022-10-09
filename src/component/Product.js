7cd59234d0014163fa29fa01db3de929e9724aee
mdashik7723 <mdashik7723@gmail.com>
2022-10-09 23:54:55 +0600

    --

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,21 +1,21 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
+import FormatPrice from "../Helpers/FormatPrice";
 
 const Product = (curElem) => {
-    const {id, name, image, price, category} = curElem;
+    const { id, name, image, price, category } = curElem;
     return (
         <NavLink to={`/singleproduct/${id}`}>
             <div className="card">
                 <figure>
-                    <img src={image} alt={name}/>
+                    <img src={image} alt={name} />
                     <figcaption className="caption">{category}</figcaption>
                 </figure>
 
                 <div className="card-data">
                     <div className="card-data-flex">
                         <h3>{name}</h3>
-                        <p className="card-data--price">{price}</p>
+                        <p className="card-data--price">{<FormatPrice price={price} />}</p>
                     </div>
                 </div>
             </div>
