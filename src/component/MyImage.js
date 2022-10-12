5d2ddcc146152b9617588914da94e782536a620f
mdashik7723 <mdashik7723@gmail.com>
2022-10-13 00:37:03 +0600

    --

--- src/component/MyImage.js
+++ src/component/MyImage.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
+import styled from "styled-components";
 
-const MyImage = () => {
+const MyImage = ({ imgs =[{url:""}] }) => {
+
+    const [mainImage, setMainImage ] = useState(imgs[0]);
     return(
-        <div>MyImage</div>
+        <>
+        <Wrapper>
+            <div className="grid grid-four-column">
+                {
+                    imgs.map((curElm, index) => {
+                        return(
+                            <figure>
+                                <img src={curElm.url} alt={curElm.filename} className="box-image--style"
+                                key={index}
+                                     onClick = {() => setMainImage(curElm)}/>
+                            </figure>
+                        );
+                    })
+                }
+            </div>
+            {/*// 2nd column*/}
+
+<div className="main-screen">
+    <img src={mainImage.url} alt={mainImage.filename}/>
+</div>
+        </Wrapper>
+        </>
     )
 };
 
 export default MyImage;
+
+const Wrapper = styled.section`
+.container {
+  padding: 9rem 0;
+}
+  .product-data {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 2rem;
+    
+    .product-data-warranty {
+      width: 100%;
+      display: flex;
+    }
+    
+  }
+`
