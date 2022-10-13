faa8cc7a826c9ccb04dcfb387b9a583b4c0785c9
mdashik7723 <mdashik7723@gmail.com>
2022-10-14 00:52:20 +0600

    --

--- src/component/Star.js
+++ src/component/Star.js
@@ -35,7 +35,25 @@ const Star = ( { stars, reviews } ) => {
 };
 
 const Wrapper = styled.section `
-
-`
+.icon-style {
+  display: flex;
+  gap: 0.2rem;
+  align-items: center;
+  justify-content: flex-start;
+}
+    .icon {
+      font-size: 2rem;
+      color: orange;
+    }
+  
+  .empty-icon {
+    font-size: 2.6rem;
+  }
+  
+  p {
+    margin: 0;
+    padding-left: 1.2rem;
+  }
+`;
 
 export default Star;
