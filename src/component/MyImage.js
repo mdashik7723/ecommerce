29aa38c77dbfafe072bc3d02594b29ae1495bb8e
mdashik7723 <mdashik7723@gmail.com>
2022-10-14 00:31:08 +0600

    --

--- src/component/MyImage.js
+++ src/component/MyImage.js
@@ -30,23 +30,48 @@ const MyImage = ({ imgs =[{url:""}] }) => {
     )
 };
 
-export default MyImage;
-
 const Wrapper = styled.section`
-.container {
-  padding: 9rem 0;
-}
-  .product-data {
+  display: grid;
+  grid-template-columns: 0.4fr 1fr;
+  gap: 1rem;
+  .grid {
+    flex-direction: row;
+    justify-items: center;
+    align-items: center;
+    width: 100%;
+    gap: 1rem;
+    /* order: 2; */
+    img {
+      max-width: 100%;
+      max-height: 100%;
+      background-size: cover;
+      object-fit: contain;
+      cursor: pointer;
+      box-shadow: ${({ theme }) => theme.colors.shadow};
+    }
+  }
+  .main-screen {
+    display: grid;
+    place-items: center;
+    order: 1;
+    img {
+      max-width: 100%;
+      height: auto;
+      box-shadow: ${({ theme }) => theme.colors.shadow};
+    }
+  }
+  .grid-four-column {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(4, 1fr);
+  }
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    gap: 2rem;
-    
-    .product-data-warranty {
-      width: 100%;
-      display: flex;
+    order: 1;
+    .grid-four-column {
+      grid-template-rows: 1fr;
+      grid-template-columns: repeat(4, 1fr);
     }
-    
   }
-`
+`;
+export default MyImage;
