a5cee9513866f2c5775549533de9b77470823ebd
mdashik7723 <mdashik7723@gmail.com>
2022-10-14 00:47:53 +0600

    --

--- src/component/Star.js
+++ src/component/Star.js
@@ -1,11 +1,41 @@
 import React from "react";
+import {AiOutlineStar, FaStar, FaStarHalf} from "react-icons/all";
+import styled from "styled-components";
 
-const Star = () => {
-    return(
-        <>
+const Star = ( { stars, reviews } ) => {
 
-        </>
-    )
+     const ratingStar = Array.from( {length: 5}, (elem,index) => {
+
+          let number = index + 0.5;
+
+          return(
+              <span key={index}>
+                  {
+                  stars >= index+1 ? <FaStar className="icon"/>
+                      :stars>= number ? <FaStarHalf className="icon"/>
+                      : <AiOutlineStar className="icon"/>
+              }
+              </span>
+          )
+
+
+
+       });
+       return(
+        <Wrapper>
+            <div className="icon-style">
+                {ratingStar}
+                <p>({reviews} customer reviews)</p>
+
+            </div>
+        </Wrapper>
+       )
+
+
 };
+
+const Wrapper = styled.section `
+
+`
 
 export default Star;
