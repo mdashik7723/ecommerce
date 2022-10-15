d2fe036cb5c8025bd0367de6b3efec6c09f8af74
mdashik7723 <mdashik7723@gmail.com>
2022-10-16 00:43:05 +0600

    --

--- src/component/AddToCart.js
+++ src/component/AddToCart.js
@@ -1,38 +1,64 @@
-import React, {useState} from "react";
+import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
-import {FaCheck} from "react-icons/all";
+import { FaCheck } from "react-icons/fa";
+import CartAmountToggle from "./CartAmountToggle";
+import { NavLink } from "react-router-dom";
+import { Button } from "../styles/Button";
 
+const AddToCart = ({ product }) => {
+    const { id, colors, stock } = product;
 
-const AddToCart = ({product}) => {
-
-    const {id, colors, stock} = product;
     const [color, setColor] = useState(colors[0]);
+    const [amount, setAmount] = useState(1);
 
-    return (< Wrapper>
-        <div className="colors">
-            <p>
-                Colors:
-                {colors.map((curColor, index) => {
-                    return (<button key={index}
-                                    style={{backgroundColor: curColor}}
-                                    className={color === curColor ? "btnStyle active" : "btnStyle"}
-                                    onClick={() => setColor(curColor)}>
-                        {color === curColor ? <FaCheck className="checkStyle"/> : null}
-                    </button>);
-                })}
-            </p>
-        </div>
-    </Wrapper>);
+    const setDecrease = () => {
+        amount > 1 ? setAmount(amount - 1) : setAmount(1);
+    };
+
+    const setIncrease = () => {
+        amount < stock ? setAmount(amount + 1) : setAmount(stock);
+    };
+
+    return (
+        <Wrapper>
+            <div className="colors">
+                <p>
+                    Color:
+                    {colors.map((curColor, index) => {
+                        return (
+                            <button
+                                key={index}
+                                style={{ backgroundColor: curColor }}
+                                className={color === curColor ? "btnStyle active" : "btnStyle"}
+                                onClick={() => setColor(curColor)}>
+                                {color === curColor ? <FaCheck className="checkStyle" /> : null}
+                            </button>
+                        );
+                    })}
+                </p>
+            </div>
+
+            {/* add to cart  */}
+            <CartAmountToggle
+                amount={amount}
+                setDecrease={setDecrease}
+                setIncrease={setIncrease}
+            />
+
+            <NavLink to="/cart">
+                <Button className="btn">Add To Cart</Button>
+            </NavLink>
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.section`
   .colors p {
-
     display: flex;
     justify-content: flex-start;
     align-items: center;
   }
-
   .btnStyle {
     width: 2rem;
     height: 2rem;
@@ -43,24 +69,18 @@ const Wrapper = styled.section`
     outline: none;
     opacity: 0.5;
     cursor: pointer;
-
     &:hover {
       opacity: 1;
     }
   }
-
   .active {
     opacity: 1;
   }
-
   .checkStyle {
-
     font-size: 1rem;
     color: #fff;
   }
-
-  //we can use it as a global one too 
-
+  /* we can use it as a global one too  */
   .amount-toggle {
     margin-top: 3rem;
     margin-bottom: 1rem;
@@ -68,18 +88,15 @@ const Wrapper = styled.section`
     justify-content: space-around;
     align-items: center;
     font-size: 1.4rem;
-
     button {
       border: none;
       background-color: #fff;
       cursor: pointer;
     }
-
     .amount-style {
       font-size: 2.4rem;
-      color: ${({theme}) => theme.colors.btn};
+      color: ${({ theme }) => theme.colors.btn};
     }
   }
 `;
-
 export default AddToCart;
