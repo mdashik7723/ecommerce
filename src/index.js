68c61d54cb7ffcf0a5453ed7f3d5e4ed655ebb7a
mdashik7723 <mdashik7723@gmail.com>
2022-10-18 00:45:42 +0600

    --

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {AppProvider} from "./context/productcontext";
+import {FilterContextProvider} from "./context/filter_context";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <AppProvider>
+        <FilterContextProvider>
         <App/>
+        </FilterContextProvider>
     </AppProvider>
+
     );
 
 // If you want to start measuring performance in your app, pass a function
