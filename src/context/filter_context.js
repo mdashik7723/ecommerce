5ae1de2c4cc3484c5bd2655d4aedc38d2e932131
mdashik7723 <mdashik7723@gmail.com>
2022-10-19 00:19:53 +0600

    --

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,27 +1,25 @@
 import React, {createContext, useContext, useEffect, useReducer} from "react";
-import { useProductContext } from "./productcontext";
+import {useProductContext} from "./productcontext";
 import reducer from "../reducer/filterReducer";
 
-
 const FilterContext = createContext();
-
 
 const initialState = {
     filter_products: [],
     all_products: [],
 };
 
-export const FilterContextProvider = ( {children} ) => {
-    const { products } = useProductContext();
+export const FilterContextProvider = ({children}) => {
+    const {products} = useProductContext();
 
-    const [state, dispatch ] = useReducer ( reducer, initialState);
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     useEffect(() => {
-        dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
-    },[products]);
+        dispatch({type: "LOAD_FILTER_PRODUCTS", payload: products});
+    }, [products]);
 
     return (
-        <FilterContext.Provider value={{ ...state }}>
+        <FilterContext.Provider value={{...state}}>
             {children}
         </FilterContext.Provider>
     )
