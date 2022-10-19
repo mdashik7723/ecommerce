a7d0de0b31ca7c7db1cf667425904bc8196ed7af
mdashik7723 <mdashik7723@gmail.com>
2022-10-20 01:16:30 +0600

    --

--- src/component/ListView.js
+++ src/component/ListView.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import styled from "styled-components";
 import FormatPrice from "../Helpers/FormatPrice";
-import { Button } from "../styles/Button";
+import {Button} from "../styles/Button";
 
-const ListView = ({ products }) => {
+const ListView = ({products}) => {
     return (
         <Wrapper className="section">
             <div className="container grid">
                 {products.map((curElem) => {
-                    const { id, name, image, price, description } = curElem;
+                    const {id, name, image, price, description} = curElem;
                     return (
                         <div className="card grid grid-two-column">
                             <figure>
-                                <img src={image} alt={name} />
+                                <img src={image} alt={name}/>
                             </figure>
 
                             <div className="card-data">
                                 <h3>{name}</h3>
                                 <p>
-                                    <FormatPrice price={price} />
+                                    <FormatPrice price={price}/>
                                 </p>
                                 <p>{description.slice(0, 90)}...</p>
 
@@ -37,12 +37,15 @@ const ListView = ({ products }) => {
 
 const Wrapper = styled.section`
   padding: 9rem 0;
+
   .container {
     max-width: 120rem;
   }
+
   .grid {
     gap: 3.2rem;
   }
+
   figure {
     width: auto;
     display: flex;
@@ -51,6 +54,7 @@ const Wrapper = styled.section`
     position: relative;
     overflow: hidden;
     transition: all 0.5s linear;
+
     &::after {
       content: "";
       position: absolute;
@@ -62,12 +66,15 @@ const Wrapper = styled.section`
       transition: all 0.2s linear;
       cursor: pointer;
     }
+
     &:hover::after {
       width: 100%;
     }
+
     &:hover img {
       transform: scale(1.2);
     }
+
     img {
       max-width: 90%;
       margin-top: 1.5rem;
@@ -75,17 +82,21 @@ const Wrapper = styled.section`
       transition: all 0.2s linear;
     }
   }
+
   .card {
     border: 0.1rem solid rgb(170 170 170 / 40%);
+
     .card-data {
       padding: 0 2rem;
     }
+
     h3 {
       margin: 2rem 0;
       font-weight: 300;
       font-size: 2.4rem;
       text-transform: capitalize;
     }
+
     .btn {
       margin: 2rem 0;
       background-color: rgb(0 0 0 / 0%);
@@ -94,17 +105,21 @@ const Wrapper = styled.section`
       justify-content: center;
       align-items: center;
       color: rgb(98 84 243);
+
       &:hover {
         background-color: rgb(98 84 243);
       }
+
       &:hover a {
         color: #fff;
       }
+
       a {
         color: rgb(98 84 243);
         font-size: 1.4rem;
       }
     }
+
     .btn-main .btn:hover {
       color: #fff;
     }
