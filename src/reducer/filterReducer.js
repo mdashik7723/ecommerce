c0471acd89a6e7a4dd1af8b4c2d31fdb1477cdae
mdashik7723 <mdashik7723@gmail.com>
2022-10-23 02:24:45 +0600

    --

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -1,14 +1,10 @@
-import React from "react";
-
 const filterReducer = (state, action) => {
-
     switch (action.type) {
-        case"LOAD_FILTER_PRODUCTS":
+        case "LOAD_FILTER_PRODUCTS":
             return {
-
                 ...state,
                 filter_products: [...action.payload],
-                all_products: [...action.payload]
+                all_products: [...action.payload],
             };
 
         case "SET_GRID_VIEW":
@@ -21,8 +17,47 @@ const filterReducer = (state, action) => {
             return {
                 ...state,
                 grid_view: false,
-            }
+            };
 
+        case "GET_SORT_VALUE":
+            // let userSortValue = document.getElementById("sort");
+            // let sort_value = userSortValue.options[userSortValue.selectedIndex].value;
+            return {
+                ...state,
+                sorting_value: action.payload,
+            };
+
+        case "SORTING_PRODUCTS":
+            let newSortData;
+            // let tempSortProduct = [...action.payload];
+
+            const { filter_products, sorting_value } = state;
+            let tempSortProduct = [...filter_products];
+
+            const sortingProducts = (a, b) => {
+                if (sorting_value === "lowest") {
+                    return a.price - b.price;
+                }
+
+                if (sorting_value === "highest") {
+                    return b.price - a.price;
+                }
+
+                if (sorting_value === "a-z") {
+                    return a.name.localeCompare(b.name);
+                }
+
+                if (sorting_value === "z-a") {
+                    return b.name.localeCompare(a.name);
+                }
+            };
+
+            newSortData = tempSortProduct.sort(sortingProducts);
+
+            return {
+                ...state,
+                filter_products: newSortData,
+            };
 
         default:
             return state;
