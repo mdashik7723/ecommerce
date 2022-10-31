c1d9b4424066c097ea79635ea2f707faf11c59ac
mdashik7723 <mdashik7723@gmail.com>
2022-11-01 01:11:32 +0600

    --

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -37,19 +37,19 @@ const filterReducer = (state, action) => {
             const sortingProducts = (a, b) => {
                 if (sorting_value === "lowest") {
                     return a.price - b.price;
-                }
+                };
 
                 if (sorting_value === "highest") {
                     return b.price - a.price;
-                }
+                };
 
                 if (sorting_value === "a-z") {
                     return a.name.localeCompare(b.name);
-                }
+                };
 
                 if (sorting_value === "z-a") {
                     return b.name.localeCompare(a.name);
-                }
+                };
             };
 
             newSortData = tempSortProduct.sort(sortingProducts);
