89ce1cf3e60b753a6d444a6a5d6d7aad46c5dbe3
mdashik7723 <mdashik7723@gmail.com>
2022-11-01 01:40:03 +0600

    --

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -59,6 +59,31 @@ const filterReducer = (state, action) => {
                 filter_products: newSortData,
             };
 
+        case "UPDATE_FILTERS_VALUE":
+            const { name, value } = action.payload;
+            return {
+                ...state,
+                filters: {
+                    ...state.filters, [name]:value,
+                },
+            };
+
+        case "FILTER_PRODUCTS":
+            let { all_products } = state;
+            let  tempFilterProduct = [...all_products];
+
+            const {text} = state.filters;
+            if (text) {
+                tempFilterProduct = tempFilterProduct.filter((curElem) => {
+                    return curElem.name.toLowerCase().includes(text);
+                })
+            }
+
+            return {
+                ...state,
+                filter_products: tempFilterProduct,
+            };
+
         default:
             return state;
     }
