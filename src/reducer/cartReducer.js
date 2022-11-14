cbea4e5b053e95566aff9b85c6cb3fec54a92e26
MD ASHIKUR RAHMAN <52170700+mdashik7723@users.noreply.github.com>
2022-11-14 10:07:51 +0600

    Update cartReducer.js
    Signed-off-by: MD ASHIKUR RAHMAN <52170700+mdashik7723@users.noreply.github.com>

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -1,10 +1,7 @@
 const cartReducer = (state, action) => {
     if (action.type === "ADD_TO_CART") {
         let { id, color, amount, product } = action.payload;
-        // console.log(
-        //   "🚀 ~ file: cartReducer.js ~ line 4 ~ cartReducer ~ product",
-        //   product
-        // );
+  
 
         let cartProduct;
 
